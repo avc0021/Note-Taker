6a80a7c800ae6b3725bb7fa790cbf0f5be8223bf
avc0021 <avc0021@gmail.com>
2022-01-24 19:46:51 -0600

    refactor,1st get route working

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+// Modules needed to project
 const fs = require('fs');
 const express = require('express');
-const path = require('path');
+
 
 
 const PORT = process.env.PORT || 3001;
@@ -14,40 +15,33 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/api/notes', (req, res) => {
-    fs.readFile("./db/db.json", "utf-8", (err, data) => {
-        const noteParse = JSON.parse(data);
-        console.log("test 1", noteParse);
-    if (noteParse == []) {
-        console.log("test 2", noteParse);
-        return res.json(noteParse);
-    } else {
-        if (err) {
-            throw err;
-        }
-    }
-  });
-});
-
-app.post("/api/notes", (req, res) => {
-    console.log(req.body);
-
-});  
+    fs.readFile("./db/db.json", "utf-8", (err,data) => {
+      const noteParse = JSON.parse(data);
+        if (err) throw err;
     
+    return res.json(noteParse);
+    });
+  });
+
+//   app.post("/api/notes", (req, res) => {
+//       console.log(req.body);
+
+//   });
+
 
 // app.delete('/api/notes', (req, res) => {
 
 // });
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-  });
+// app.get("/", (req, res) => {
+//     res.sendFile(path.join(__dirname, "./public/index.html"));
+//   });
 
 
-app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
-  });
+// app.get("/notes", (req, res) => {
+//     res.sendFile(path.join(__dirname, "./public/notes.html"));
+//   });
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
-  
